f31006fa17e8d60869018f6a064302769a8bc1b8
cjdvnd0206 <cjdvndaudnjf@naver.com>
2022-05-10 10:43:07 +0900

    Initial Commit

---
+++ RealmMemo/RealmMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmMemoApp.swift
+//  RealmMemo
+//
+//  Created by user on 2022/05/10.
+//
+
+import SwiftUI
+
+@main
+struct RealmMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
